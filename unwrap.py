b3f0ba88dc84ad79b70e5626d9c12c0aa6e8e463
Alexey Zankevich <zancudero@gmail.com>
2016-02-12 19:24:15 +0300

    updated source map array

--- unwrap.py
+++ unwrap.py
@@ -44,32 +44,40 @@ class Main():
         self.load_image()
         self.load_points()
         self.draw_mask()
-        self.calc_mask()
-        self.save_image()
 
-    def calc_mask(self):
         col_count = 30
         row_count = 20
+        map1 = self.calc_source_map(col_count, row_count)
+        map2 = self.calc_dest_map(col_count, row_count)
+        self.save_image()
 
+    def calc_dest_map(self, col_count, row_count):
+        rows = []
+        for i in range(col_count):
+            for j in range(row_count):
+                pass
+        return rows
+
+    def calc_source_map(self, col_count, row_count):
         top_points = self.calc_ellipse_points(self.point_a, self.point_b, self.point_c, col_count)
         bottom_points = self.calc_ellipse_points(self.point_d, self.point_e, self.point_f, col_count)
 
-        cols = []
-        for i in range(col_count):
-            top_point = top_points[i]
-            bottom_point = bottom_points[i]
+        rows = []
+        for row_index in range(row_count):
+            row = []
+            for col_index in range(col_count):
+                top_point = top_points[col_index]
+                bottom_point = bottom_points[col_index]
 
-            delta = (top_point - bottom_point) / float(row_count - 1)
+                delta = (top_point - bottom_point) / float(row_count - 1)
 
-            col = []
-            for j in range(row_count):
-                point = top_point - delta * j
-                col.append(point)
-
+                point = top_point - delta * row_index
+                row.append(point)
                 x, y = map(int, point)
-                cv2.line(self.image, (x, y), (x, y), color=self.YELLOW_COLOR, thickness=3)
 
-            cols.append(col)
+                cv2.line(self.image, (x, y), (x, y), color=self.YELLOW_COLOR, thickness=3)
+            rows.append(row)
+        return rows
 
     def draw_poly_mask(self, color=WHITE_COLOR):
         cv2.polylines(self.image, np.int32([self.points]), 1, color)
@@ -98,7 +106,7 @@ class Main():
 
         cv2.ellipse(self.image, center_point, axis, angle, start_angle, end_angle, color=color)
 
-    def calc_ellipse_points(self, left, top, right, points_count=30):
+    def calc_ellipse_points(self, left, top, right, points_count):
         center = (left + right) / 2
 
         # get ellipse axis
@@ -124,9 +132,10 @@ class Main():
             x = int(dx + center[0])
             y = int(dy + center[1])
 
-            cv2.line(self.image, (x, y), (x, y), color=self.YELLOW_COLOR, thickness=3)
+            # cv2.line(self.image, (x, y), (x, y), color=self.YELLOW_COLOR, thickness=3)
             points.append([x, y])
 
+        points.reverse()
         return np.array(points)
 
     def get_ellipse_point(self, a, b, phi):
@@ -134,6 +143,28 @@ class Main():
         Get ellipse radius in polar coordinates
         """
         return a * np.cos(phi), b * np.sin(phi)
+
+
+class Main2(object):
+    def run(self):
+        import cv2
+        from scipy.interpolate import griddata
+
+        grid_x, grid_y = np.mgrid[0:149:150j, 0:149:150j]
+        destination = np.array([[0,0], [0,49], [0,99], [0,149],
+                          [49,0],[49,49],[49,99],[49,149],
+                          [99,0],[99,49],[99,99],[99,149],
+                          [149,0],[149,49],[149,99],[149,149]])
+        source = np.array([[22,22], [24,68], [26,116], [25,162],
+                          [64,19],[65,64],[65,114],[64,159],
+                          [107,16],[108,62],[108,111],[107,157],
+                          [151,11],[151,58],[151,107],[151,156]])
+        grid_z = griddata(destination, source, (grid_x, grid_y), method='cubic')
+        map_x = np.append([], [ar[:,1] for ar in grid_z]).reshape(150,150)
+        map_y = np.append([], [ar[:,0] for ar in grid_z]).reshape(150,150)
+        map_x_32 = map_x.astype('float32')
+        map_y_32 = map_y.astype('float32')
+
 
 
 if __name__ == '__main__':
