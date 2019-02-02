e41799b60391063e000dcae3303a2705b161623f
Alexey Zankevich <alex.zankevich@gmail.com>
2019-02-01 20:47:33 -0500

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+__author__ = 'Alexey Zankevich'
+
+
+setup(
+    name="unwrap_labels",
+    version="1.0.1",
+    py_modules=['unwrap_labels'],
+    author="Alexey Zankevich",
+    author_email="alex.zankevich@gmail.com",
+    description="Library to unwrap labels using OpenCV",
+    keywords=["OpenCV"],
+    license="MIT",
+    platforms=['Platform Independent'],
+    url="https://github.com/Nepherhotep/unwrap_labels",
+    install_requires=['numpy'],
+    classifiers=["Development Status :: 5 - Production/Stable",
+                 "Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3.5",
+                 "Programming Language :: Python :: 3.6",
+                 "Programming Language :: Python :: 3.7"]
+)
